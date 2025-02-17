f3f66e6789927021f87ecf4812290628c59a0d79
Serdar Demirkol <aoserdardemirkol@gmail.com>
2025-02-17 13:32:45 +0300

    -adds consumer

---
+++ ProductsMicroservice/src/main/java/com/aosd/ws/products/service/ProductServiceImpl.java
@@ -0,0 +1,85 @@
+package com.aosd.ws.products.service;
+
+import com.aosd.ws.core.ProductCreatedEvent;
+import com.aosd.ws.products.rest.CreateProductRestModel;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public String createProductAsync(CreateProductRestModel productRestModel) {
+
+        String productId = UUID.randomUUID().toString();
+
+        // TODO: Persist Product Details into database table before publishing an Event
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,
+                productRestModel.getTitle(),
+                productRestModel.getPrice(),
+                productRestModel.getQuantity());
+
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future =
+                kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+
+        future.whenComplete((result, exception) -> {
+            if (exception != null) {
+                LOGGER.error("********** Failed to send message: {}", exception.getMessage());
+            } else {
+                LOGGER.info("********** Message sent successfully: {}", result.getRecordMetadata());
+            }
+        });
+
+        // to make it synchronized -> future.join();
+
+        LOGGER.info("********** Returning product id {}", productId);
+
+        return productId;
+    }
+
+    @Override
+    public String createProductSync(CreateProductRestModel productRestModel) throws Exception {
+
+        String productId = UUID.randomUUID().toString();
+
+        // TODO: Persist Product Details into database table before publishing an Event
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,
+                productRestModel.getTitle(),
+                productRestModel.getPrice(),
+                productRestModel.getQuantity());
+
+        LOGGER.info("********** Before publishig a ProductCreatedEvent");
+
+        ProducerRecord<String, ProductCreatedEvent> record =
+                new ProducerRecord<>("product-created-events-topic", productId, productCreatedEvent);
+        record.headers().add("messageId", UUID.randomUUID().toString().getBytes());
+
+        SendResult<String, ProductCreatedEvent> result =
+                kafkaTemplate.send(record).get();
+
+        LOGGER.info("********** Partition: {}", result.getRecordMetadata().partition());
+        LOGGER.info("********** Topic {}", result.getRecordMetadata().topic());
+        LOGGER.info("********** Offset {}", result.getRecordMetadata().offset());
+
+        LOGGER.info("********** Returning product id {}", productId);
+
+        return productId;
+    }
+}
